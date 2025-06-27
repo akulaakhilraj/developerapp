0fe5d44f6e574be120f0e31e1cf7b9ee6afbfe8c
Akhil <akulaakhilraj@gmail.com>
2025-06-26 21:47:15 -0400

    server changes

--- src/app.js
+++ src/app.js
@@ -1,16 +1,12 @@
 const express=require("express");
-
 const app=express();
 
+//Handling request
 app.use((req,res)=>{
-    res.send("server is noted")
+    res.send("server is restarted and running smoothly on the dev")
 })
-
 
 //Listen
 app.listen(9000,()=>{
     console.log("server listening on 9000")
 })
-
-
-
