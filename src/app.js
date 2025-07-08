505ae52a2072f231d75e0c4768c22613a07a4b19
Akhil <akulaakhilraj@gmail.com>
2025-07-08 16:13:48 -0400

    multiple git handlers

--- src/app.js
+++ src/app.js
@@ -1,12 +1,36 @@
 const express=require("express");
-const app=express();
+const app= express()
 
-//Handling request
-app.use((req,res)=>{
-    res.send("server is restarted and running smoothly on the dev")
+//Get the User Details 
+app.get("/user", (req,res,next)=>{
+    console.log("First Response")
+   // res.send({FirstName:"Akhil Raj", LastName:"Akula", MobileNo:8886285838, email_id:"akulaakhilraj@gmail.com"})
+    next();
+},(req,res,next)=>{
+    console.log("2nd Response")
+   res.send("2nd Response Read")
+    next();
 })
 
-//Listen
-app.listen(9000,()=>{
-    console.log("server listening on 9000")
+//Post User Details
+
+app.post("/user",(req,res)=>{
+    
+    res.send({FirstName:"Akula", LastName:"Akhil Raj", MobileNo:9058473822, email_id:"akulaakhil@gmail.com"}
+    )
 })
+
+app.delete("/user",(req,res)=>{
+    res.send({FirstName:"Akula Akhil", LastName:"Akhil Raj", MobileNo:9058473822, email_id:"akulaakhil@gmail.com"}
+    )
+})
+
+
+
+const PORT=9000;
+
+app.listen(PORT, ()=>{
+    console.log(`Server is listening on the ${PORT}`
+    )
+})
+
