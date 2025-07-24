d1bc32b0cc9e4a0275a2dc96a9462d3f2870e231
Akhil <akulaakhilraj@gmail.com>
2025-07-23 22:25:04 -0400

    data stagination

--- src/app.js
+++ src/app.js
@@ -1,27 +1,88 @@
 const express=require("express");
 const connectDB=require("./config/database");
-const User=require("./models/user")
+const User=require("./models/user");
+const user = require("./models/user");
 //const connec=require("./config/database")
 const app= express();
 const PORT=9000;
 
-app.post("/signup", async(req,res,next)=>{
-    const user=new User({
-    firstName:"Akhil Raj",
-    lastName:"Akula",
-    emailId:"akulaakhilraj1997@gmail.com",
-    age:28,
-    gender:"male"
-})
-try{
-     await user.save()
-    res.status(200).send("User data is succesfully done")
+//Which connect req body fields
+app.use(express.json())
 
+app.post("/signup", async(req,res)=>{
+    console.log("Sign up doc ",req.body)
+    const user=new User(req.body)
+try{
+    await user.save()
+    console.log('USER', user);
+    res.status(200).send("User data is succesfully done")
 }
 catch(err){
-    res.status(401).send("Getting error", err.message);
+   // console.error("Validation Error:", err.message);
+    res.status(400).send({ error: err.message });
 }
-   
+})
+
+app.get("/feed", async(req,res)=>{
+    const UserEmailId=req.body.emailId;
+    try{
+        const users=await User.find({emailId:UserEmailId});
+        if(users.length===0){
+            res.status(404).send("Nothing is Found");
+        }
+        else{
+            res.send(users);
+        }
+    }
+    catch(err){
+        res.status(404).send("User not found ")
+    }
+})
+
+app.patch("/user/:userId", async(req,res)=>{
+    const userId=req.params?.userId; //checks the user req holds the parameter userid
+    console.log("userId", userId);
+    console.log("UserId:",userId);
+    const data=req.body;
+    console.log("data:", data)
+    try{
+        const ISALLOWED_UPDATE=["userId","photoUrl","skills", "gender","age","about"]
+        const isAllowedUpdate=Object.keys(data).every((k)=>ISALLOWED_UPDATE.includes(k))
+        console.log("IsAllowedUpdate:", isAllowedUpdate);
+        if(!isAllowedUpdate){
+           throw new Error("Update is not allowed now")
+        }
+        if(data?.skills.length>10){
+            throw new Error("Skills cannot be more than 10");
+        }
+
+       const user= await User.findByIdAndUpdate({_id:userId}, data, { 
+        returnDocument:"after",
+       runValidators:true
+       })
+        res.send("Successfully updated")
+    }
+    catch(err){
+        res.status(400).send("Something went wrong"+ err.message)
+    }
+})
+
+
+
+
+
+
+
+app.delete("/user", async(req,res)=>{
+    const userId=req.body.userId
+    console.log("Received userId:", req.body.userId);
+    try{
+        const users=await User.findByIdAndDelete(userId)
+        console.log(users)
+        res.send("User Id is deleted");
+    }catch(err){
+        res.status(404).send("Nothing is found")
+    }
 })
 
 connectDB().then(()=>{
--- src/models/user.js
+++ src/models/user.js
@@ -1,22 +1,78 @@
 const mongoose=require("mongoose");
+const validator=require("validator");
 
 const userSchema= new mongoose.Schema({
     firstName:{
-        type:String
+        type:String,
+        required:true,
+        minLength:4,
+        maxLength:50,
     },
     lastName:{
-        type:String
+        type:String,
+        required:true,
+        minLength:4,
+        maxLength:40,
     },
     emailId:{
-        type:String
-    },
+        type:String,
+        required:true,
+        lowercase:true,
+        trim:true,
+        unique:true,  //No duplicate
+        validate(value){
+           if(!validator.isEmail(value)){
+            throw new Error("Need a valid Email id")
+           }
+        }
+    }, 
     age:{
-        type:Number
+        type:String,
+        min:18,
+    },
+    
+    PhoneNo: {
+    type:String,
+    required:true,
+    validate(value) {
+    if (!validator.isMobilePhone(value)) {
+      throw new Error("Need a valid Phone Number");
+    }
+  }
+},
+    password:{
+        type:String,
+         validate(value){
+           if(!validator.isStrongPassword(value)){
+            throw new Error("Need a Strong Valid Password");
+           }
+        }
     },
     gender:{
-        type:String
+        type:String,
+        validate(value){
+            if(!["male", "female", "others"].includes(value)){
+                throw new Error ("Gender validation is not valid")
+            }
+        }
+    },
+    photoUrl:{
+        type:String,
+        default:"https://tse3.mm.bing.net/th/id/OIP.DIL3U7SxMklew2-3p82DXQHaHa?pid=Api&P=0&h=220",
+         validate(value){
+           if(!validator.isURL(value)){
+            throw new Error("Need a valid  PhotoUrl");
+           }
+        }
+    },
+    about:{
+        type:String,
+        default:"Update with about skills",
+    },
+    skills:{
+        type:[String]
     }
-})
+}, {timestamps:true})
 
 module.exports=mongoose.model("User", userSchema)
 
